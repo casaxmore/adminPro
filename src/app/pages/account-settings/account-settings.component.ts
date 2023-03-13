6ceaaf6e4fe8170627fe92620968b88342e2bbc2
casaxmore <casaxmore@gmail.com>
2023-03-13 20:39:17 +0100

    menu lateral

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SettingsService } from '../../services/settings.service';
 
 @Component({
   selector: 'app-account-settings',
@@ -8,34 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountSettingsComponent implements OnInit{
 
-  public linkTheme = document.querySelector('#theme');
-  public links = document.querySelectorAll('.selector');
+
+
+  constructor(private settingsService: SettingsService){}
 
   ngOnInit(): void {
-    this.checkCurrentTheme();
-    this.links = document.querySelectorAll('.selector');
+    this.settingsService.checkCurrentTheme();
   }
 
   changeTheme(theme:string){
-    const url = `./assets/css/colors/${theme}.css`;
-
-    this.linkTheme?.setAttribute('href', url);
-    localStorage.setItem('theme', url);
-    this.checkCurrentTheme();
+    this.settingsService.changeTheme(theme);
   }
 
-  checkCurrentTheme() {
-
-    this.links.forEach(elem => {
-      elem.classList.remove('working');
-      const btnTheme = elem.getAttribute('data-theme');
-      const btnThemeUrl = `./assets/css/colors/${btnTheme}.css`;
-      const currentTheme = this.linkTheme?.getAttribute('href');
-
-      if( btnThemeUrl === currentTheme) {
-        elem.classList.add('working');
-      }
-    })
-  }
 
 }
