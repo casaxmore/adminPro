5711caa985625a68cb2f5b18c999a80492f32411
casaxmore <casaxmore@gmail.com>
2023-03-27 22:42:48 +0200

    LocalStorage de login y registro

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,17 +1,53 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+
+import { UsuarioService } from 'src/app/services/usuario.service';
+import { LoginForm } from 'src/app/interfaces/login-form.interface';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    private usuarioService: UsuarioService
+  ) {}
 
-  constructor(private router: Router){}
+  public formSubmitted = false;
+
+  public loginForm = this.fb.group({
+    email: [ localStorage.getItem('email') || '', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+    remember: [false],
+  });
 
   login() {
-    this.router.navigateByUrl('/');
-  }
+    /* this.router.navigateByUrl('/'); */
+    this.formSubmitted = true;
+    console.log(this.loginForm.value);
 
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    // Login
+    this.usuarioService.login(this.loginForm.value).subscribe(
+      (resp) => {
+        if ( this.loginForm.get('remember')!.value) {
+          localStorage.setItem('email', this.loginForm.get('email')!.value!);
+        } else {
+          localStorage.removeItem('email');
+        }
+      },
+      (err) => {
+        // si sucede un error
+        Swal.fire('Error', err.error.msg, 'error');
+      }
+    );
+  }
 }
--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,19 +1,34 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+// Lo que hace es lanzar un efecto secundario, paso adicional
+import { tap } from 'rxjs/operators';
+
 import { environment } from '../environments/environment';
+import { LoginForm } from '../interfaces/login-form.interface';
 import { RegisterForm } from '../interfaces/register-form.interface';
 
 const base_url = environment.base_url;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsuarioService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  crearUsuario(formData: RegisterForm){
+  crearUsuario(formData: RegisterForm) {
     console.log('Creando usuario', formData.nombre);
-    return this.http.post(`${base_url}/usuarios`, formData);
+    return this.http.post(`${base_url}/usuarios`, formData).pipe(
+      tap((resp: any) => {
+        localStorage.setItem('tokenCrear', resp.token);
+      })
+    );
+  }
+
+  login(formData: any) {
+    return this.http.post(`${base_url}/login`, formData).pipe(
+      tap((resp: any) => {
+        localStorage.setItem('tokenLogin', resp.token);
+      })
+    );
   }
 }
