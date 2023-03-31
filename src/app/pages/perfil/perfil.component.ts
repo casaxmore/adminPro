04a8047ab3a12f56c31927a587c46515c017151f
casaxmore <casaxmore@gmail.com>
2023-03-31 19:12:46 +0200

    cambiar imagen de usuario

--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Usuario } from 'src/app/models/usuario.model';
 import { UsuarioService } from '../../services/usuario.service';
+import Swal from 'sweetalert2'
+import { FileUploadService } from '../../services/file-upload.service';
 
 @Component({
   selector: 'app-perfil',
@@ -10,21 +13,46 @@ import { UsuarioService } from '../../services/usuario.service';
 export class PerfilComponent implements OnInit{
 
   public perfilForm!: FormGroup;
+  public usuario!: Usuario;
+  public imagenSubir!: File;
 
-  constructor(private fb: FormBuilder, private usuarioSerive: UsuarioService) {}
+  constructor(private fb: FormBuilder, private usuarioSerive: UsuarioService, private fileUploadService: FileUploadService) {
+    this.usuario = usuarioSerive.usuario;
+  }
 
   ngOnInit(): void {
     this.perfilForm = this.fb.group({
-      nombre: ['123', Validators.required],
-      email: ['abc', [Validators.required, Validators.email]]
+      nombre: [this.usuario.nombre, Validators.required],
+      email: [this.usuario.email, [Validators.required, Validators.email]]
     });
   }
 
   actualizarPerfil() {
     console.log(this.perfilForm.value);
-    this.usuarioSerive.actualizarPerfil(this.perfilForm.value).subscribe(resp => {
-      console.log(resp);
+    this.usuarioSerive.actualizarPerfil(this.perfilForm.value).subscribe(() => {
+      const {nombre, email} = this.perfilForm.value;
+      this.usuario.nombre = nombre;
+      this.usuario.email = email;
+      Swal.fire(
+        'Usuario modificado',
+        '',
+        'success'
+      )
     })
+  }
+
+  cambiarImagen(event: any){
+    var file : File;
+
+    file = event.target.files[0];
+    console.log(file);
+
+    this.imagenSubir = file;
+  }
+
+  subirImagen() {
+    this.fileUploadService.actualizarFoto(this.imagenSubir, 'usuarios', this.usuario.uid)
+    .then(img => console.log(img));
   }
 
 }
