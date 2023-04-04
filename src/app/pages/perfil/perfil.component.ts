5f4d3b8b0cb59e97c5a2ecf80d48ab0223ac3428
casaxmore <casaxmore@gmail.com>
2023-04-04 17:29:08 +0200

    validaciones usuarios google

--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -2,69 +2,78 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Usuario } from 'src/app/models/usuario.model';
 import { UsuarioService } from '../../services/usuario.service';
-import Swal from 'sweetalert2'
+import Swal from 'sweetalert2';
 import { FileUploadService } from '../../services/file-upload.service';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
-  styleUrls: ['./perfil.component.css']
+  styleUrls: ['./perfil.component.css'],
 })
-export class PerfilComponent implements OnInit{
-
+export class PerfilComponent implements OnInit {
   public perfilForm!: FormGroup;
   public usuario!: Usuario;
   public imagenSubir!: File;
   public imgTemp: any = '';
 
-  constructor(private fb: FormBuilder, private usuarioSerive: UsuarioService, private fileUploadService: FileUploadService) {
+  constructor(
+    private fb: FormBuilder,
+    private usuarioSerive: UsuarioService,
+    private fileUploadService: FileUploadService
+  ) {
     this.usuario = usuarioSerive.usuario;
   }
 
   ngOnInit(): void {
     this.perfilForm = this.fb.group({
       nombre: [this.usuario.nombre, Validators.required],
-      email: [this.usuario.email, [Validators.required, Validators.email]]
+      email: [this.usuario.email, [Validators.required, Validators.email]],
     });
   }
 
   actualizarPerfil() {
     console.log(this.perfilForm.value);
-    this.usuarioSerive.actualizarPerfil(this.perfilForm.value).subscribe(() => {
-      const {nombre, email} = this.perfilForm.value;
-      this.usuario.nombre = nombre;
-      this.usuario.email = email;
-      Swal.fire(
-        'Usuario modificado',
-        '',
-        'success'
-      )
-    })
+    this.usuarioSerive.actualizarPerfil(this.perfilForm.value).subscribe(
+      () => {
+        const { nombre, email } = this.perfilForm.value;
+        this.usuario.nombre = nombre;
+        this.usuario.email = email;
+        Swal.fire('Guardado', 'Los cambios fueron guardados', 'success');
+      },
+      (err) => {
+        Swal.fire('Error', err.error.msg, 'error');
+      }
+    );
   }
 
-  cambiarImagen(event: any): any{
-    var file : File;
+  cambiarImagen(event: any): any {
+    var file: File;
 
     file = event.target.files[0];
     console.log(file);
 
     this.imagenSubir = file;
 
-    if (!file){
-      return this.imgTemp = null;
+    if (!file) {
+      return (this.imgTemp = null);
     }
     const reader = new FileReader();
-    const url64 = reader. readAsDataURL(file);
+    const url64 = reader.readAsDataURL(file);
 
     reader.onloadend = () => {
       this.imgTemp = reader.result;
       console.log(reader.result);
-    }
+    };
   }
 
   subirImagen() {
-    this.fileUploadService.actualizarFoto(this.imagenSubir, 'usuarios', this.usuario.uid)
-    .then(img => this.usuario.img = img);
+    this.fileUploadService
+      .actualizarFoto(this.imagenSubir, 'usuarios', this.usuario.uid)
+      .then((img) => {
+        this.usuario.img = img;
+        Swal.fire('Imagen guardada', 'Imagen de usuario actualizada', 'success');
+      }).catch(err => {
+        Swal.fire('Error', err.error.msg, 'error');
+      });
   }
-
 }
